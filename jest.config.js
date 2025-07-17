ce6d52afdf29792ee094c4a1aa0ca30f4999720b
Glestman <ramonglestman2.0@gmail.com>
2025-07-17 20:05:34 -0300

    continuidade de desenvolvimento

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: ['node_modules/(?!(some-esm-package)/)'], // só se usar pacote ESM
+};
